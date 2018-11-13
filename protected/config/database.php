fce55565b66681f42aa9847cba885056b1cef059
liuchaofan <liuchaofan@xiaomi.com>
2018-11-13 09:53:08 +0800

    社区活动页为

--- protected/config/database.php
+++ protected/config/database.php
@@ -5,6 +5,13 @@ return array(
 //	'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
 	// uncomment the following lines to use a MySQL database
 
+//	'connectionString' => 'mysql:host=39.106.197.228;dbname=jxhd',
+//	'emulatePrepare' => true,
+//	'username' => 'root',
+//	'password' => 'jxhd_test',
+//	'charset' => 'utf8',
+//
+
 	'connectionString' => 'mysql:host=JXSQ-rm-bp1fg2aan1t7v675h.mysql.rds.aliyuncs.com:3306;dbname=jxhd',
 	'emulatePrepare' => true,
 	'username' => 'root',
